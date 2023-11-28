e98439da6d95b13451342eb00860656b220f5695
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2023-11-28 09:20:15 -0300

    curso-angular-loiane

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,34 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
+import { ExemploNgContentComponent } from './exemplo-ng-content/exemplo-ng-content.component';
+import { OperadorElvisComponent } from './operador-elvis/operador-elvis.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiretivaNgclassComponent,
+    DiretivaNgforComponent,
+    DiretivaNgifComponent,
+    DiretivaNgstyleComponent,
+    DiretivaNgswitchComponent,
+    DiretivasCustomizadasComponent,
+    ExemploNgContentComponent,
+    OperadorElvisComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
